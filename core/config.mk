cc7fc4c3d09fca4f6644b7b563da6c50c3abef4b
Luca Stefani <luca020400@lineageos.org>
2017-08-18 10:23:00 +0200

    core: Move lineage sepolicy to device/

--- core/config.mk
+++ core/config.mk
@@ -911,7 +911,7 @@ endif
 ifneq ($(LINEAGE_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/lineage/sepolicy/sepolicy.mk)
+$(eval include device/lineage/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
